36e3ef461fb0e37d61ce2e3b9a1f91f6f9518386
Quiver92 <Quiver92@mail.ru>
2016-10-01 11:05:36 +0300

    add a menu in setup_manager.sh

--- setup_manager.sh
+++ setup_manager.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
-echo "==========================================="
-echo "Menu to Setup"
-echo "==========================================="
-echo "Enter 1 to setup teamviewer"
-echo "Enter 2 to setup skype"
-echo "Enter q to exit"
-echo -e "\n"
-echo -e "Enter your selection:"
-read answer
+tput clear
+tput setaf 3
+echo "Chameleon Soft"
+tput sgr0
+
+#Title
+(tput sc ; tput cup 5 17 ; tput bold ; echo "====================================" ; tput rc)
+(tput sc ; tput cup 6 27 ; tput bold ; echo "Menu to Setup" ; tput rc)
+(tput sc ; tput cup 7 17 ; tput bold ; echo "====================================" ; tput rc)
+#Choices
+(tput cup 9 5 ; tput setaf 4 ; echo "Enter 1 to setup teamviewer")
+(tput cup 10 5 ; tput setaf 4 ; echo "Enter 2 to setup skype" )
+(tput cup 12 5 ; tput setaf 5 ; echo "Enter q to exit"; tput cup 13 5 ; echo -e "\n" ; tput cup 14 5 ; echo -e "Enter your selection:")
+
+( tput bold ; tput cup 15 5 ; read -p "Enter your choice [1-2]" answer)
+
+tput clear
+tput sgr0
+rput rc
+
 case "$answer" in
+#Setup teamviewer
 1) sudo rm -rf /mnt/teamviewer* && sudo apt-get install -f && sudo apt-get -y install libxtst6:i386 libxrandr2:i386 && sudo apt-get update && sudo apt-get install -f && cd /mnt/ && sudo wget https://download.teamviewer.com/download/teamviewer_i386.deb && cd /mnt && sudo dpkg -i /mnt/teamviewer* && sudo rm -rf /mnt/teamviever* ;;
+
+#Setup skype
 2) sudo apt-get install -f && sudo apt-get install sni-qt:i386 libdbusmenu-qt2:i386 libqt4-dbus:i386 libxss1:i386 && sudo apt-get update && sudo apt-get install libgtk2.0-0:i386 gtk2-engines:i386 libgconf-2-4:i386 && sudo apt-get install -f && sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner" && sudo apt-get update && sudo apt-get install skype ;;
 q) exit ;;
 esac
 ##Установка Chameleon
-#sudo chmod 777 /etc/apt/sources.list  && echo "deb http://repos.chmsoft.com.ua/ xenial-evolution-2.5 non-free">>/etc/apt/sources.list && sudo apt-key adv --recv-keys --keyserver keys.gnupg.net 1FB50E9B838F7C88 && sudo apt-get install -y chameleon-client && sudo apt-get install chameleon-client-database && sudo apt-get install chameleon-spring && sudo apt-get install chameleon-gui-plugin-all
+#sudo chmod 775 /etc/apt/sources.list  && sudo echo "deb http://repos.chmsoft.com.ua/ xenial-evolution-2.5 non-free">>/etc/apt/sources.list && sudo apt-key adv --recv-keys --keyserver keys.gnupg.net 1FB50E9B838F7C88 && sudo apt-get install -y chameleon-client chameleon-client-database chameleon-spring install chameleon-gui-plugin-all
 
 
 
